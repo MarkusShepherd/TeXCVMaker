46fe189c0c0fe31911bda29042cdfc25d4548ac5
Markus Schepke <mschepke@yahoo-inc.com>
2014-10-08 10:31:21 +0100

    modified run.sh to be independent of the location

--- run.sh
+++ run.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
 IN=$1
 TEX=${IN%.*}.tex
+OUTDIR=`dirname "$1"`
 
-java -jar lib/saxon9he.jar -s:$IN -xsl:xslt/xml2tex.xsl -o:$TEX
-pdflatex $TEX
-pdflatex $TEX
+java -jar "$DIR/lib/saxon9he.jar" -s:"$IN" -xsl:"$DIR/xslt/xml2tex.xsl" -o:"$TEX" || exit 1
+pdflatex -output-directory "$OUTDIR" "$TEX" || exit 1
+pdflatex -output-directory "$OUTDIR" "$TEX" || exit 1
 
